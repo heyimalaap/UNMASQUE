cde8e679638d18af4ac513499deba39d90464518
Ahana <sendatahana@gmail.com>
2023-09-13 15:38:50 +0530

    formatting changes

--- mysite/unmasque/src/main_cmd.py
+++ mysite/unmasque/src/main_cmd.py
@@ -1,12 +1,11 @@
-from mysite.unmasque.src.core import ExtractionPipeLine
+from core import ExtractionPipeLine
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     hq = "select c_mktsegment, l_orderkey, sum(l_extendedprice) as revenue, " \
-              "o_orderdate, o_shippriority from customer, orders, lineitem where c_custkey = o_custkey " \
-              "and l_orderkey = o_orderkey and o_orderdate > date '1995-10-11' " \
-              "group by l_orderkey, o_orderdate, o_shippriority, c_mktsegment limit 4;"
-
+         "o_orderdate, o_shippriority from customer, orders, lineitem where c_custkey = o_custkey " \
+         "and l_orderkey = o_orderkey and o_orderdate > date '1995-10-11' " \
+         "group by l_orderkey, o_orderdate, o_shippriority, c_mktsegment limit 4;"
 
     eq, time = ExtractionPipeLine.extract(hq)
 
